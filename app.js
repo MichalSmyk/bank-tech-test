609ab6265bab4783f2998dd4419e18355f7f77ab
MichalSmyk <michal.smyk39@gmail.com>
2023-02-20 15:28:35 +0000

    add app.js to see how the code works

---
+++ app.js
@@ -0,0 +1,10 @@
+const BankModel = require('./lib/model');
+const BankStatement = require('./lib/view'); 
+
+const statement = new BankStatement();
+
+statement.deposit('10/01/2023', 100);
+statement.deposit('13/01/2023', 200);
+statement.withdraw('14/01/2023', 50);
+
+console.log(statement.statement());
