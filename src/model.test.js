a46f591b28486816f3e4965518b3c24425472e93
MichalSmyk <michal.smyk39@gmail.com>
2023-02-20 14:18:39 +0000

    deposit method working

--- src/model.test.js
+++ src/model.test.js
@@ -9,7 +9,13 @@ describe('BankModel', () => {
     it('returns date', () => {
         expect(newBankModel.getDate()).toBe('02/01/2023')
     });
+
     it('returns an amount', () => {
         expect(newBankModel.getAmount()).toBe(100)
     });
+
+    it('returns total amount after withdrawal', () => {
+        expect(newBankModel.getDeposit(50)).toBe(50)
+    })
+
 })
