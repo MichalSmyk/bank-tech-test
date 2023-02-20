c9da13e1957607e53845a45a08a9bc3e9e442b55
MichalSmyk <michal.smyk39@gmail.com>
2023-02-20 14:06:55 +0000

    bank model returns amount

--- src/model.test.js
+++ src/model.test.js
@@ -5,4 +5,8 @@ describe('BankModel', () => {
         const newBankModel = new BankModel('02/01/2023', 100);
         expect(newBankModel.getDate()).toBe('02/01/2023')
     })
+    it('returns an amount', () => {
+        const newBankModel = new BankModel('02/01/2023', 100)
+        expect(newBankModel.getAmount()).toBe(100)
+    })
 })
