6f6e7117df2fbd9186563fa8550e8b0a88c82bb2
MichalSmyk <michal.smyk39@gmail.com>
2023-02-20 14:55:46 +0000

    add withdraw to transaction array

--- src/view.test.js
+++ src/view.test.js
@@ -10,8 +10,15 @@ describe('BankStatement', () => {
     it('has an empty transactions array', () => {
         expect(statement.transactions).toEqual([]);
     });
+
     it('adds deposit to transactions array', () => {
         statement.deposit('01/02/2023', 100)
         expect(statement.transactions).toEqual([{ date: '01/02/2023', amount: 100}]);
     });
+
+    it('adds a withdraw to transactions array', () => {
+        statement.withdraw('02/02/2023', 50)
+        expect(statement.transactions).toEqual([{ date: '02/02/2023', amount: -50}]);
+    });
+
 })
