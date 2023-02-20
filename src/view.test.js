bbbc3253aca1e3d2bb2584518c1e98405af1bdfe
MichalSmyk <michal.smyk39@gmail.com>
2023-02-20 14:50:20 +0000

    deposit method in view class working

--- src/view.test.js
+++ src/view.test.js
@@ -10,4 +10,8 @@ describe('BankStatement', () => {
     it('has an empty transactions array', () => {
         expect(statement.transactions).toEqual([]);
     });
+    it('adds deposit to transactions array', () => {
+        statement.deposit('01/02/2023', 100)
+        expect(statement.transactions).toEqual([{ date: '01/02/2023', amount: 100}]);
+    });
 })
